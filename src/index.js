dbb61055df44f72f011e235db63325dc8a9c2618
Jackson.Jiang <jackson.jiang@outlook.com>
2018-07-05 15:43:56 +0800

    feat(test): 添加测试框架

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const teams = require('./teams.json');
 
 module.exports = {
   all: _.clone(teams),
+  first: teams[0],
   champion() {
     const i = Math.floor(Math.random() * teams.length);
     return teams[i];
---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+var fifa = require('./index.js')
+var expect =require('chai').expect;
+
+describe('jks-opensource', function() {
+  describe('all', function() {
+    it('一共4只球队', function() {
+      expect(fifa.all.length).to.equal(4);
+    });
+    it('必须有`巴西`队', function() {
+      expect(fifa.all).to.include('巴西');
+    });
+  });
+  describe('champion', function() {
+    it('冠军球队在所有队伍中', function() {
+      expect(fifa.all).to.include(fifa.champion());
+    });
+  });
+});
